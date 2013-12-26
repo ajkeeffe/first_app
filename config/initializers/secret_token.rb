bbe6edf4fb6b9124db1224f94be9f0f7db6e7a86
Andrew Keeffe <andrew.keeffe@alaska.gov>
2013-12-25 21:26:57 -0900

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rtest::Application.config.secret_key_base = '73e7a76932694dfbdd245f0ead5c0f2a8767e503a4bda74fd3b07e1725cd116d3d2191d7f997bf54dd6d19d65d059d6ffd9f0074499a1e42900a14514d5a94c0'
